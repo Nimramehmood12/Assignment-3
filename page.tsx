7e0f9f1d77a4f707b41200ea3c378911d114b555
Nimra Memood <162594962+Nimramehmood12@users.noreply.github.com>
2024-10-02 17:53:28 +0500

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,10 +1,7 @@
-import Header from "./components/header";
-import Footer from "./components/footer";
-export default function Home(){
-  return(
-    <div>
-      <Header></Header>
-      <Footer></Footer>
-    </div>
-  )
-}
+import footer from "@/app/components/footer";
+
+export default function (){
+     return(
+        <div></div>
+     )
+    }
